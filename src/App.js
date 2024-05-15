28cc733a638f7d1d63320ae5cf48e2d637356197
Rehtse-16 <hehstarr@gmail.com>
2024-05-15 10:42:30 +0100

    Change date to 17/05/2024

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./Timer.css";
 import Timer from './components/Timer';
 
 function App() {
-  const eventDate = new Date("2024-01-01T00:00:00").getTime();
+  const eventDate = new Date("2024-05-17T00:00:00").getTime();
   return (
     <>
       <Timer eventDate={eventDate}/>
